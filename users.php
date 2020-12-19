fb081611d669f312a4bc1a676694ad4fb6244d29
Raimundo Jose <jhraimundo3@gmail.com>
2020-12-19 13:39:52 +0200

    list

--- users.php
+++ users.php
@@ -20,19 +20,11 @@ $query = mysqli_query($con, 'SELECT * FROM  products');
     while ($row=mysqli_fetch_array($query))
     {
           $user_id=$row['codigo_producto'];
-          $fullname=$row['nombre_producto']." / ".$row["stock"];
-          $user_name=$row['precio_producto'];
+          $details=$row['nombre_producto'];
+          $stock=$row['stock'];
+          $preco=$row['precio_producto'];
         
-          $vector[] = array('user_id'=>$user_id,'fullname'=>$fullname,'user'=>$user_name);
+          $vector[] = array('id'=>$user_id,'details'=>$details,'preco'=>$preco, 'stock'=>$stock);
     }
 
 echo json_encode($vector);
-
-
-
-/*
-        $array = array (['user_id'=>'test1', 'fullname'=> 'test2', 'user'=> 'test3'],
-		['user_id'=>'test11', 'fullname'=> 'test22', 'user'=> 'test33']);
-        echo json_encode($array);
-		
-		*/
